d92518be61d143bb509c99a7f432cda5a1d7d268
IEVN1003-21000453 <rodriguezmedinaemiliano982@gmail.com>
2025-09-05 18:50:44 -0600

    Segundo Commit Ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import './style.css'
+import './temasTs/01-tipos-bas.ts'
+
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
---
+++ src/temasTs/01-tipos-bas.ts
@@ -0,0 +1,19 @@
+console.log("Hola Mundo")
+
+var num1=23;
+let num2=32;
+const num3=67;
+
+let nombre:string
+let num:number
+let activo:boolean
+
+let sum = num1+num2+num3;
+console.log("La suma es : "+sum);
+
+let matricula:number|string
+
+matricula=12345678
+matricula='21000453'
+console.log(matricula)
+
